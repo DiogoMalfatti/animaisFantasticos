67a81b25e2d4839745cff0dbb206657070635ba9
Diogo Malfatti <diimalfatti@gmail.com>
2024-12-04 14:55:48 -0300

    separando em modules

---
+++ js/script.js
@@ -0,0 +1,9 @@
+import initTabNav from "./modules/tabNav.js";
+import initAccordion from "./modules/accordion.js";
+import initScrollSuave from "./modules/scrollSuave.js";
+import initAnimacaoScroll from "./modules/animaScroll.js";
+
+initTabNav();
+initAccordion();
+initScrollSuave();
+initAnimacaoScroll();

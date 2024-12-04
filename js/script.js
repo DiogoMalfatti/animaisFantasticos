3dd6700ecf2b82ee97fcbf48f4b14d47d8884333
Diogo Malfatti <diimalfatti@gmail.com>
2024-12-04 16:18:17 -0300

    criado modal

--- js/script.js
+++ js/script.js
@@ -2,8 +2,10 @@ import initTabNav from "./modules/tabNav.js";
 import initAccordion from "./modules/accordion.js";
 import initScrollSuave from "./modules/scrollSuave.js";
 import initAnimacaoScroll from "./modules/animaScroll.js";
+import initModal from "./modules/modal.js";
 
 initTabNav();
 initAccordion();
 initScrollSuave();
 initAnimacaoScroll();
+initModal();

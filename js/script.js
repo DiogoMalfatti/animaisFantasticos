d921fd35e940668f4fc6c377650dea67d0899e8a
Diogo Malfatti <diimalfatti@gmail.com>
2024-12-05 09:38:52 -0300

    criado tooltip

--- js/script.js
+++ js/script.js
@@ -3,9 +3,11 @@ import initAccordion from "./modules/accordion.js";
 import initScrollSuave from "./modules/scrollSuave.js";
 import initAnimacaoScroll from "./modules/animaScroll.js";
 import initModal from "./modules/modal.js";
+import initTooltip from "./modules/tooltip.js";
 
 initTabNav();
 initAccordion();
 initScrollSuave();
 initAnimacaoScroll();
 initModal();
+initTooltip();

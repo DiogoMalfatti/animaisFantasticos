b56db180d088525fcb632d7944c56b83b7d4da1a
Diogo Malfatti <diimalfatti@gmail.com>
2024-12-06 09:56:33 -0300

    horario funcionamento

--- js/script.js
+++ js/script.js
@@ -7,6 +7,7 @@ import initTooltip from "./modules/tooltip.js";
 import initDropdownMenu from "./modules/dropdownMenu.js";
 import initMenuMobile from "./modules/menuMobile.js";
 import initAnimaNumeros from "./modules/animaNumeros.js";
+import initFuncionamento from "./modules/funcionamento.js";
 
 initTabNav();
 initAccordion();
@@ -17,3 +18,4 @@ initTooltip();
 initDropdownMenu();
 initMenuMobile();
 initAnimaNumeros();
+initFuncionamento();
